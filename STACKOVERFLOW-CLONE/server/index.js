9645da54dd8b1c29c980fae0ffa94df3bef3822a
Vishusing <vs299901@gmail.com>
2024-04-30 15:23:31 +0530

    Removed some depricated values

--- STACKOVERFLOW-CLONE/server/index.js
+++ STACKOVERFLOW-CLONE/server/index.js
@@ -16,8 +16,8 @@ app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("This is a stack overflow clone API")
-})
-
+}) 
+ 
 app.use('/user', userRoutes)
 app.use('/questions', questionRoutes)
 app.use('/answer', answerRoutes)
@@ -26,6 +26,6 @@ const PORT = process.env.PORT || 5000
 
 const DATABASE_URL = process.env.CONNECTION_URL
 
-mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(DATABASE_URL)
     .then(() => app.listen(PORT, () => { console.log(`server running on port ${PORT}`) }))
     .catch((err) => console.log(err.message))
