a9a628de22b5a8fdd0678b84059fede6a5a4150c
Vishusing <vs299901@gmail.com>
2024-06-26 20:31:22 +0530

    deleted folder

--- STACKOVERFLOW-CLONE/client/src/components/index.js
+++
@@ -1,7 +0,0 @@
-import LeftSidebar from "./LeftSidebar/LeftSidebar";
-import RigtSidebar from "./RightSidebar/RightSidebar";
-import HomeMainbar from "./HomeMainbar/HomeMainbar";
-import Navbar from "./Navbar/Navbar";
-import Avatar from "./Avatar/Avatar";
-
-export { LeftSidebar, RigtSidebar, HomeMainbar, Navbar, Avatar }
