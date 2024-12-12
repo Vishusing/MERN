2d36d14e0bccb81632fd2975d4bb7c92ff9ba88f
Vishusing <vs299901@gmail.com>
2024-12-12 20:32:12 +0530

    .

--- restAPI/app.js
+++ restAPI/app.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.get('/', (req, res) => {
     res.send("Hii I am live");
 });
-
+ 
 app.use('/api/products', product_routes);
 
 const start = async () => {
